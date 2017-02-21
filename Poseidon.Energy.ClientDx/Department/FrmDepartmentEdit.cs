afa3335571f6865ffbf91dc7fc7217d7ca912264
robertzml <robertzml@126.com>
2017-02-21 11:18:39 +0800

    Add FrmDepartmentEdit.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Poseidon.Energy.ClientDx/Department/FrmDepartmentEdit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poseidon.Energy.ClientDx
+{
+    using Poseidon.Base.Framework;
+    using Poseidon.Base.System;
+    using Poseidon.Energy.Core.BL;
+    using Poseidon.Energy.Core.DL;
+    using Poseidon.Winform.Base;
+
+    public partial class FrmDepartmentEdit : BaseSingleForm
+    {
+        public FrmDepartmentEdit()
+        {
+            InitializeComponent();
+        }
+    }
+}
