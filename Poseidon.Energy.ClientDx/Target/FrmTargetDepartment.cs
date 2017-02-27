503f705311fd0b19646fb66636d413690ff7f542
robertzml <robertzml@126.com>
2017-02-27 23:03:58 +0800

    Add FrmTargetDepartment.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Poseidon.Energy.ClientDx/Target/FrmTargetDepartment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poseidon.Energy.ClientDx
+{
+    using Poseidon.Base.Framework;
+    using Poseidon.Core.BL;
+    using Poseidon.Core.DL;
+    using Poseidon.Winform.Base;
+    using Poseidon.Energy.Core.BL;
+    using Poseidon.Energy.Core.DL;
+
+    /// <summary>
+    /// 部门指标制定窗体
+    /// </summary>
+    public partial class FrmTargetDepartment : BaseMdiForm
+    {
+        public FrmTargetDepartment()
+        {
+            InitializeComponent();
+        }
+    }
+}
